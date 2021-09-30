28127ee6889b0fefc9d02493cc2b191360972914
Aqib Javed <aqibj76@gmail.com>
2021-09-30 16:21:29 +0500

    Update CalculatorAppTests.swift

--- CalculatorApp/CalculatorAppTests/CalculatorAppTests.swift
+++ CalculatorApp/CalculatorAppTests/CalculatorAppTests.swift
@@ -27,6 +27,54 @@ class CalculatorAppTests: XCTestCase {
         XCTAssertEqual(sum, num1+num2)
     }
     
+    func testSubtract () {
+        var num1 : Double = 10
+        var num2 : Double = 20
+        let calculator = Calculator(first: num1, second: num2)
+        
+        let result = calculator.subtraction()
+        
+        
+        
+        XCTAssertEqual(result, num1-num2)
+        
+        num2 = 10
+        num1 = 100
+        XCTAssertEqual(result, num1-num2)
+    }
+    
+    func testMultiply () {
+        var num1 : Double = 10
+        var num2 : Double = 20
+        let calculator = Calculator(first: num1, second: num2)
+        
+        let result = calculator.multiplication()
+        
+        
+        
+        XCTAssertEqual(result, num1*num2)
+        
+        num2 = 10
+        num1 = 100
+        XCTAssertEqual(result, num1*num2)
+    }
+    
+    func testDivision () {
+        var num1 : Double = 10
+        var num2 : Double = 20
+        let calculator = Calculator(first: num1, second: num2)
+        
+        let result = calculator.divison()
+        
+        
+        
+        XCTAssertEqual(result, num1/num2)
+        
+        num2 = 10
+        num1 = 100
+        XCTAssertEqual(result, num1/num2)
+    }
+    
     
 
 }
